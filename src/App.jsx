20271c984f53c43b62db790d9a2c19e6ece27347
V empire <thomas.addisu@aastustudent.edu.et>
2026-04-20 19:02:45 +0300

    setup application routing with protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/common/Navbar";
+import ProtectedRoute from "./components/common/ProtectedRoute";
 
-
+const Login = () => <div>Login</div>;
+const Driver = () => <div>Driver</div>;
+const Admin = () => <div>Admin</div>;
+const Station = () => <div>Station</div>;
 
 function App() {
-  return <h1>FuelPass Frontend Running 🚀</h1>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/driver/dashboard"
+          element={
+            <ProtectedRoute role="driver">
+              <Driver />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute role="admin">
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/station/scanner"
+          element={
+            <ProtectedRoute role="staff">
+              <Station />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
