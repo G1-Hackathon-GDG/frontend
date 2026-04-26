18bf929321dff9f39f954694184346cd8d1c6eba
Fikerte Yimer <fikerteyimer06gmail.com>
2026-04-26 17:55:02 +0300

    integrate SocketProvider into app root and ensure proper context hierarchy for auth and socket communication

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { Socket } from "socket.io-client";
+import { SocketProvider } from "./context/SocketContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
+        <SocketProvider>
         <App />
+        </SocketProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
